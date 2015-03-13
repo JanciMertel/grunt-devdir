f2d5c19ad54faf7c063b6d2c5e039990f44bd5df
Janči <johnny.mert@gmail.com>
2015-03-13 18:49:57 +0100

    asynchronous tasks + errors

--- tasks/devdir_end.js
+++ tasks/devdir_end.js
@@ -10,34 +10,58 @@
 
 var fs = require('fs');
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-    grunt.registerMultiTask('devdir_end', 'Grunt plugin for switching between specified dev / prod files', function() {
+    grunt.registerMultiTask('devdir_end', 'Grunt plugin for switching between specified dev / prod files', function () {
         // Merge task-specific and/or target-specific options with these defaults.
-        var options = this.options({
-            punctuation: '.',
-            separator: ', '
-        });
-        if(typeof this.data.disabled !== "undefined" && !this.data.disabled)
-        {
-            return;
+        var options = this.options({ });
+        if (typeof this.data.disabled !== "undefined" && !this.data.disabled) {
+            return true;
         }
+
+        var self = this;
         var fromBase = null;
         var toBase = null;
         var fromBaseExt = null;
         var toBaseExt = null;
-        for(var file in this.data)
-        {
-            if(typeof this.data[file].from !== "undefined" && typeof this.data[file].to !== "undefined")
-            {
+        var files = Object.keys(this.data).length;
+        var current = 0;
+
+        var done = this.async();
+
+        for (var file in this.data) {
+            if (typeof this.data[file].from !== "undefined" && typeof this.data[file].to !== "undefined") {
                 fromBase = this.data[file].from.split(".");
                 fromBaseExt = fromBase.pop();
                 fromBase = fromBase.join('.');
                 toBase = this.data[file].to.split(".");
                 toBaseExt = toBase.pop();
                 toBase = toBase.join('.');
-                fs.rename(this.data[file].from, this.data[file].to, function (err) {});
-                fs.rename(fromBase + "_start." + fromBaseExt, this.data[file].from, function (err) {});
+                fs.rename(self.data[file].from, self.data[file].to, function (err) {
+                    if(err)
+                    {
+                        grunt.log.writeln('').write('Cannot rename the first file\n'.red);
+                        done(false);
+                    }
+                    else
+                    {
+                        fs.rename(fromBase + "_start." + fromBaseExt, self.data[file].from, function (err) {
+                            if(err)
+                            {
+                                grunt.log.writeln('').write('Cannot rename the second file\n'.red);
+                                done(false);
+                            }
+                            else
+                            {
+                                current++;
+                                if(current == files)
+                                {
+                                    done(true);
+                                }
+                            }
+                        });
+                    }
+                });
             }
         }
     });
--- tasks/devdir_start.js
+++ tasks/devdir_start.js
@@ -10,36 +10,61 @@
 
 var fs = require('fs');
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  grunt.registerMultiTask('devdir_start', 'Grunt plugin for switching between specified dev / prod files', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
-    if(typeof this.data.disabled !== "undefined" && !this.data.disabled)
-    {
-        return;
-    }
-    var fromBase = null;
-    var toBase = null;
-    var fromBaseExt = null;
-    var toBaseExt = null;
-    for(var file in this.data)
-    {
-        if(typeof this.data[file].from !== "undefined" && typeof this.data[file].to !== "undefined")
-        {
-            fromBase = this.data[file].from.split(".");
-            fromBaseExt = fromBase.pop();
-            fromBase = fromBase.join('.');
-            toBase = this.data[file].to.split(".");
-            toBaseExt = toBase.pop();
-            toBase = toBase.join('.');
-            fs.rename(this.data[file].from, fromBase + "_start." + fromBaseExt, function (err) {});
-            fs.rename(this.data[file].to, this.data[file].from, function (err) {});
+    grunt.registerMultiTask('devdir_start', 'Grunt plugin for switching between specified dev / prod files', function () {
+        // Merge task-specific and/or target-specific options with these defaults.
+        var options = this.options({ });
+        if (typeof this.data.disabled !== "undefined" && !this.data.disabled) {
+            return true;
         }
-    }
-  });
+
+        var self = this;
+        var fromBase = null;
+        var toBase = null;
+        var fromBaseExt = null;
+        var toBaseExt = null;
+        var files = Object.keys(this.data).length;
+        var current = 0;
+
+        var done = this.async();
+
+        for (var file in this.data) {
+            if (typeof this.data[file].from !== "undefined" && typeof this.data[file].to !== "undefined") {
+                fromBase = this.data[file].from.split(".");
+                fromBaseExt = fromBase.pop();
+                fromBase = fromBase.join('.');
+                toBase = this.data[file].to.split(".");
+                toBaseExt = toBase.pop();
+                toBase = toBase.join('.');
+                fs.rename(self.data[file].from, fromBase + "_start." + fromBaseExt, function (err) {
+                    if(err)
+                    {
+                        grunt.log.writeln('').write('Cannot rename the first file\n'.red);
+                        done(false);
+                    }
+                    else
+                    {
+                        fs.rename(self.data[file].to, self.data[file].from, function (err) {
+                            if(err)
+                            {
+                                grunt.log.writeln('').write('Cannot rename the second file\n'.red);
+                                done(false);
+                            }
+                            else
+                            {
+                                current++;
+
+                                if(current == files)
+                                {
+                                    done(true);
+                                }
+                            }
+                        });
+                    }
+                });
+            }
+        }
+    });
 
 };
